c8486d77ba4d0eef544741506e3ee03e778187bd
redupkr <reduardo1994@hotmail.com>
2020-11-12 01:18:19 -0300

    Modelos de bebidas

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
     class Meta:
-        db_table = 'ingredients'
+        db_table = 'ingredient'
 
 class IngredientValue(models.Model):
     value = models.DecimalField(max_digits=5, decimal_places=2, localize=True)
@@ -13,7 +13,7 @@ class IngredientValue(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'ingredients_value'
+        db_table = 'ingredient_value'
 
 class Sandwich(models.Model):
     name = models.CharField(max_length=50)
@@ -36,3 +36,22 @@ class SandwichIngredient(models.Model):
 
     class Meta:
         db_table = 'sandwich_ingredient'
+
+class Beverage(models.Model):
+    name = models.CharField(max_length=50)
+    size = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'beverage'
+
+class BeverageValue(models.Model):
+    value = models.DecimalField(max_digits=5, decimal_places=2, localize=True)
+    date = models.DateField()
+
+    beverage = models.ForeignKey(Beverage, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'beverage_value'
+
+
+
