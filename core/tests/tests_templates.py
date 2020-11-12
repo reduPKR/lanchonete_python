13c144802faa4bdeaab03ded8f3e123023b01c5d
redupkr <reduardo1994@hotmail.com>
2020-11-12 19:09:57 -0300

    Testes menu ingredient

--- core/tests/tests_templates.py
+++ core/tests/tests_templates.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 class TemplatesViewTests(TestCase):
 
+    #Home
     def test_template_home_status_ok(self):
         response = self.client.get(reverse("home_view"))
         self.assertEqual(response.status_code, 200)
@@ -11,3 +12,12 @@ class TemplatesViewTests(TestCase):
     def test_template_home(self):
         response = self.client.get(reverse("home_view"))
         self.assertTemplateUsed(response, "home.html")
+
+    #Ingredientes
+    def test_menu_ingredient_status_ok(self):
+        response = self.client.get(reverse("menu_ingredient_view"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_menu_ingredient_(self):
+        response = self.client.get(reverse("menu_ingredient_view"))
+        self.assertTemplateUsed(response, "ingredient/menu.html")
