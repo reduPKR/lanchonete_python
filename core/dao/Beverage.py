66b6f994c4a954e18bdd8e9841a00e46f52a21fd
redupkr <reduardo1994@hotmail.com>
2020-11-25 01:20:02 -0300

    teste criate beverage passando

--- core/dao/Beverage.py
+++ core/dao/Beverage.py
@@ -1,10 +1,21 @@
 from core import models
+from datetime import date
 
 def create(name, size, value):
     try:
-        try_create(name, size, value)
+        return try_create(name, size, value)
     except:
         return None
 
-def try_create(name, size):
-    pass
+def try_create(name, size, value):
+    beverage = models.Beverage.objects.create(name=name, size=size)
+    add_price(beverage, value)
+
+    return beverage
+
+def add_price(beverage, value):
+    models.BeverageValue.objects.create(
+        beverage=beverage,
+        value=value,
+        date=date.today()
+    )
